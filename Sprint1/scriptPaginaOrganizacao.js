bc2c39f205511c733748ddcfc44ef656b080a3ce
Alejandro <ale_huller@hotmail.com>
2023-03-11 15:49:40 -0300

    Codigos Iniciais Front

---
+++ Sprint1/scriptPaginaOrganizacao.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.modal-container');
+const tbody = document.querySelector('tbody');
+const sNome = document.querySelector('#m-nome');
+const sEmail = document.querySelector('#m-email');
+const sStatus = document.querySelector('#m-status');
+const sGrupo = document.querySelector('#m-grupo');
+const btnSalvar = document.querySelector('#btnSalvar');
+
+let itens;
+let id;
