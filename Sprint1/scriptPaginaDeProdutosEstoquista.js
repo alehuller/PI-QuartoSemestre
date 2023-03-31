81ba8e8d956ceeac0cb3c63be2d072842c6a1222
Alejandro <ale_huller@hotmail.com>
2023-03-31 12:26:52 -0300

    adicoes sprint2

--- Sprint1/scriptPaginaDeProdutosEstoquista.js
+++ Sprint1/scriptPaginaDeProdutosEstoquista.js
@@ -1,12 +1,14 @@
 const modal = document.querySelector('.modal-container');
 const tbody = document.querySelector('tbody');
+const sCodigo = document.querySelector('#m-codigo');
 const sNome = document.querySelector('#m-nome');
-const sCpf = document.querySelector('#m-cpf');
-const sEmail = document.querySelector('#m-email');
+const sDescricao = document.querySelector('#m-descricao');
+const sAvaliacao = document.querySelector('#m-avaliacao');
+const sPreco = document.querySelector('#m-preco');
+const sQuantidade = document.querySelector('#m-quantidade');
 const sStatus = document.querySelector('#m-status');
-const sGrupo = document.querySelector('#m-grupo');
-const sSenha = document.querySelector('#m-senha');
-const sSenhaConfirm = document.querySelector('#m-senhaConfirm');
+const sLink = document.querySelector('#m-link');
+const sImagens = document.querySelector('#m-imagens');
 //variaveis que indicam atributos de um item
 const btnSalvar = document.querySelector('#btnSalvar');
 
@@ -14,48 +16,6 @@ let itens; //variavel que armazena os itens
 let id; //armazena o index para edicao
 
 const itemArray = [];
-
-function validarEmail(email) { //funcao para validar o cadastro (se nao houver cadastro com outro email)
-  //criar um array com os emails e verificar eles
-  let emailRepetido = "";
-  for(let y = 0; y <= itemArray.length; y++) {
-    if(email == itemArray[y]) {
-      emailRepetido = email;
-    }
-    else {
-      emailRepetido = " ";
-    }
-  }
-
-  if(emailRepetido == email) {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-
-function contaCpf(strCpf) { //funcao para validar o CPF
-  var soma;
-  var resto;
-  soma = 0;
-  
-  if (strCpf == "000000000") return false;
-
-  for (i = 1; i <= 9; i++) soma = soma + parseInt(strCpf.substring(i-1, i)) * (11-i);
-  resto = (soma * 10) % 11;
-
-  if ((resto == 10) || (resto == 11)) resto = 0;
-  if (resto != parseInt(strCpf.substring(9, 10))) return false;
-
-  soma = 0;
-  for (i = 1; i <= 10; i++) soma = soma + parseInt(strCpf.substring(i - 1, i)) * (12 - i);
-  resto = (soma * 10) % 11;
-
-  if((resto == 10) || (resto == 11)) resto = 0;
-  if(resto != parseInt(strCpf.substring(10, 11))) return false;
-  return true;
-}
 
 function openModal(edit = false, index = 0) { //funcao onde aparece a tela de inclusao. tudo false para nao aparecer na tela a parte do incluir.
     modal.classList.add('active') //a modal eh ativada para ficar visivel em tela
@@ -67,22 +27,10 @@ function openModal(edit = false, index = 0) { //funcao onde aparece a tela de in
     }
 
     if (edit) { //valores que sao editados na inclusao, cada novo com seu index
-        sNome.value = itens[index].nome
-        sSenha.value = itens[index].senha
-        sSenhaConfirm.value = itens[index].sSenhaConfirm
-        sCpf.value = itens[index].cpf
-        sEmail.value = itens[index].email
-        sStatus.value = itens[index].status
-        sGrupo.value = itens[index].grupo
+        sQuantidade.value = itens[index].quantidade
         id = index
     } else { //valores nao preenchidos
-      sNome.value = ''
-      sSenha.value = ''
-      sSenhaConfirm.value = ''
-      sCpf.value = ''
-      sEmail.value = ''
-      sStatus.value = ''
-      sGrupo.value = ''
+      sQuantidade.value = ''
     }
     
 }
@@ -102,11 +50,12 @@ function insertItem(item, index) { //funcao onde junta e insere os atributos do 
     let tr = document.createElement('tr')
   
     tr.innerHTML = `
+      <td>${item.codigo}</td>
       <td>${item.nome}</td>
-      <td>${item.cpf}</td>
-      <td>${item.email}</td>
+      <td>${item.quantidade}</td>
+      <td>${item.preco}</td>
       <td>${item.status} <input id="alterarStatus" type="button" onclick="alterar()" value="Alterar" /></td>
-      <td>${item.grupo}</td>
+      <td>${item.link}</td>
       <td class="acao">
         <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button> 
       </td>
@@ -116,26 +65,20 @@ function insertItem(item, index) { //funcao onde junta e insere os atributos do 
 
 btnSalvar.onclick = e => { //botao de salvar os dados da inclusao
 
-  if (sNome.value == '' || sSenha.value == '' || sSenhaConfirm.value == '' || sCpf.value == '' || sEmail.value == '' || sGrupo.value == '' || sStatus.value == '') { //valores do item
+  if (sQuantidade.value == '') { //valores do item
     return
   }
   
   e.preventDefault();
 
-  if(sSenha.value === sSenhaConfirm.value && contaCpf(sCpf.value) && validarEmail(sEmail.value)) {
+  
     if (id !== undefined) { //atribui os parametros passados para suas variaveis e id
-    itens[id].nome = sNome.value
-    itens[id].senha = sSenha.value
-    itens[id].senhaConfirm = sSenhaConfirm.value
-    itens[id].cpf = sCpf.value
-    itens[id].email = sEmail.value
-    itens[id].grupo = sGrupo.value
-    itens[id].status = sStatus.value
+    itens[id].quantidade = sQuantidade.value
     for(let varFor = 0; varFor <= itemArray.length; i++) {
       itemArray[i] = sStatus.value;
     }
     } else {
-      itens.push({'nome': sNome.value, 'senha': sSenha.value, 'senhaConfirm': sSenhaConfirm.value, 'cpf': sCpf.value, 'email': sEmail.value, 'grupo': sGrupo.value, 'status': sStatus.value})
+      itens.push({'quantidade': sQuantidade.value})
     }
   
     setItensBD() //seta os novos itens no banco de dados
@@ -144,7 +87,6 @@ btnSalvar.onclick = e => { //botao de salvar os dados da inclusao
     loadItens() //carrega novamente com o novo item criado
     id = undefined
   }
-}
 
 function loadItens() { //funcao para buscar os itens do banco e colocar na tela quando carregada
     itens = getItensBD()
@@ -172,7 +114,8 @@ function alterar() {
 }
 
   
-const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? [] //funcao que pega o item no banco local. o JSON.parse torna uma informacao em formato JSON em um objeto JavaScript
-const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens)) //seta os itens no banco local em JSON form
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfuncc')) ?? [] //funcao que pega o item no banco local. o JSON.parse torna uma informacao em formato JSON em um objeto JavaScript
+const setItensBD = () => localStorage.setItem('dbfuncc', JSON.stringify(itens)) //seta os itens no banco local em JSON form
   
 loadItens()
+
